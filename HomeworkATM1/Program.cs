c5b8b20a88db2d82c09cfd3d1e62c0151911348b
Gary Mullins <gmullins925@outlook.com>
2019-09-08 23:18:45 -0500

    Initial Commit

---
+++ HomeworkATM1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkATM1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int balance;
+			string accountName;
+			int menuOption = 0;
+			Checking checking = null;
+			Savings savings = null;
+
+			while (menuOption != 1)
+			{
+				menuOption = GetNumber("1 - Exit\n2 - Create Checking\n3 - Create Savings\n4 - Account Balance");
+				switch (menuOption)
+				{
+
+					case 1:
+						break;
+
+					case 2:
+						accountName = GetInput("Account Name");
+						balance = GetNumber("Initial Amt: ");
+						checking = new Checking(accountName, balance, 0.02f);
+						break;
+
+					case 3:
+						accountName = GetInput("Account Name");
+						balance = GetNumber("Initial Amt: ");
+						savings = new Savings(accountName, balance, 0.02f);
+						break;
+
+					case 4:
+						accountName = GetInput("Account Name");
+						Console.WriteLine(prompt.Balance);
+						break;
+
+					default:
+						break;
+				}
+			}
+		}   
+
+		public static string GetInput(string prompt)
+		{
+			Console.Write(prompt);
+			string str = Console.ReadLine();
+			return str;
+		}  
+
+		public static int GetNumber(string prompt)
+		{
+			int userNumber;
+			string strNumber = GetInput(prompt);
+			while (!Int32.TryParse(strNumber, out userNumber))
+			{
+				Console.WriteLine("That is not an integer");
+				strNumber = GetInput(prompt);
+			}
+			return userNumber;
+		}  
+	}
+}
---
+++ HomeworkATM1/Savings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkATM1
+{
+	class Savings : Account
+	{
+		public Savings(string accountName, decimal balance, float interestRate) : base(accountName, balance, interestRate)
+		{
+			//      gift of a $100 for opening an new account
+			this.Deposit(100);
+		}
+	}
+}
